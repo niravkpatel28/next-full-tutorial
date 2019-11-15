dbc34df91fdfa6b384ba8ee11313cefef5da70b8
niravkpatel28 <niravkpatel28@gmail.com>
2019-11-15 14:54:12 +0530

    Fetching data and rendering pages  dynamically

---
+++ 05-fetching-data/component/Layout.js
@@ -0,0 +1,44 @@
+//this is a higher order component that will contain a header and footer
+//each page or route will have this layout. 
+// this is simply created by using props.children mechanism where the 
+// component can access the jsx within the start and ending tags using props.children
+
+import Link from 'next/link';
+import Head from 'next/head';
+const Layout = props => {
+    const linkStyle = {
+      marginRight: 15
+    };
+  return (
+    <>
+      <Head>
+        <title> NEXT js Sample App</title>
+      </Head>
+      <header>
+        <nav>
+          <Link href="/">
+            <a style={linkStyle}> Home </a>
+          </Link>
+          <span> {"     "}</span>
+          <Link href="/aboutus">
+            <a style={linkStyle}> About Us </a>
+          </Link>
+          <Link href="/users">
+            <a style={linkStyle}> Users </a>
+          </Link>
+        </nav>
+      </header>
+      <hr />
+      {props.children}
+      <hr />
+      <footer>
+        <p> Some footer &copy; {new Date().getFullYear()} </p>
+      </footer>
+      <style jsx>{`
+        //    add style that is locally scoped
+      `}</style>
+    </>
+  );
+};
+
+export default Layout
---
+++ 05-fetching-data/pages/aboutus.js
@@ -0,0 +1,13 @@
+import Layout from '../component/Layout';
+const AboutUs = ()=>{
+
+    return(
+        <>
+            <Layout>
+                <h1> About  Us Page</h1>
+            </Layout>
+        </>
+    )
+}
+
+export default AboutUs;
