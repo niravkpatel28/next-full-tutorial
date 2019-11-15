ce4976cf38053fb4e56cf6afd44cf914776e6c1f
niravkpatel28 <niravkpatel28@gmail.com>
2019-11-15 11:26:44 +0530

    Creating Shared Component

---
+++ 02-client-side-navigation/pages/aboutus.js
@@ -0,0 +1,13 @@
+import Layout from '../comps/Layout';
+const AboutUs = ()=>{
+
+    return(
+        <>
+            <Layout>
+                <h1> About  Us Page</h1>
+            </Layout>
+        </>
+    )
+}
+
+export default AboutUs;
---
+++ 02-client-side-navigation/pages/index.js
@@ -0,0 +1,13 @@
+
+import Layout from '../comps/Layout'
+const Index = ()=>{
+    return (
+      <>
+        <Layout>
+          <h1> Index Page </h1>
+        </Layout>
+      </>
+    );
+}
+
+export default Index ;
